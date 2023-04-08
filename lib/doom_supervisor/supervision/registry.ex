ca3df11d1f326996783f155c4f82e5d2b36b5dba
André Albuquerque <andre.malbuq@gmail.com>
2023-04-08 16:48:08 +0100

    Log Registry info messages

--- lib/doom_supervisor/supervision/registry.ex
+++ lib/doom_supervisor/supervision/registry.ex
@@ -5,13 +5,21 @@ defmodule DoomSupervisor.Supervision.Registry do
   - A process was killed (since its name was unregistered)
   """
 
+  require Logger
+
   @registry_name Application.compile_env(:doom_supervisor, :registry_name)
 
   def register_name(name, pid) do
+    Logger.info("Registering #{inspect(pid)} as #{inspect(name)}...")
+
     Registry.register_name({@registry_name, name}, pid)
   end
 
   def unregister_name(name) do
+    pid = whereis_name(name)
+
+    Logger.info("*Un*registering #{inspect(pid)} as #{inspect(name)}...")
+
     Registry.unregister_name({@registry_name, name})
   end
 
